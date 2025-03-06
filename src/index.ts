49e06f52db4913fd83c7af93d5dfb57debadbdd9
Oluwatomisin Adeyinka Tomori <oluwatomisin@Oluwatomisin.local>
2025-03-06 22:03:28 +0100

    fix: safari cookie not setting

--- src/index.ts
+++ src/index.ts
@@ -94,7 +94,7 @@ async function bootstrap() {
       proxy: false,
       cookie: {
         maxAge: 1000 * 60 * 60 * 24,
-        sameSite: isProduction ? 'none' : 'lax',
+        sameSite: isProduction ? 'lax' : 'lax',
         secure: isProduction,
         httpOnly: true,
       },
